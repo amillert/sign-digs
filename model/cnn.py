768b688316312e371064035c8b3b4dff4d88dc1d
amillert <devmood.business@gmail.com>
2022-02-24 13:58:06 +0100

    update cnn model

--- model/cnn.py
+++ model/cnn.py
@@ -6,28 +6,20 @@ class SignDigitsCNN(nn.Module):
         super().__init__()
         self.network = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # output: 64 x 32 x 32
+            nn.MaxPool2d(2, 2),
             ##
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # output: 128 x 16 x 16
+            nn.MaxPool2d(2, 2),
             ##
             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # output: 256 x 8 x 8
+            nn.MaxPool2d(2, 2),
             ##
             nn.Flatten(),
             nn.Linear(16384, 8192),
-            nn.ReLU(),
             nn.Linear(8192, 1024),
-            nn.ReLU(),
             nn.Linear(1024, 10),
         )
 

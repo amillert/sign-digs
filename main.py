019c9f36db2f8f01915293f5fa2f8665fa51fbe1
amillert <devmood.business@gmail.com>
2022-02-24 12:43:41 +0100

    add argparser and restructure main loop

--- main.py
+++ main.py
@@ -9,35 +9,46 @@ from torch.utils.data import DataLoader, random_split
 from model.cnn import SignDigitsCNN
 import util
 from util.preprocess import CustomImageDataset
+from util.argparser import get_cli_args
+
+
+def main() -> None:
+    args, subparser = get_cli_args()
+
+    if subparser == "train":
+
+        datapath = os.path.abspath(args.in_path)
+        dataset = CustomImageDataset(datapath)
+        data_len = len(dataset)
+        test_size = int((1 - args.train_size) * data_len)
+        train_size = data_len - test_size
+
+        trainset, testset = random_split(dataset, [train_size, test_size])
+
+        BATCH_SIZE = args.batch_size
+        CPUs = os.cpu_count()
+        train_dl = DataLoader(trainset, BATCH_SIZE, True, num_workers=CPUs)
+        test_dl = DataLoader(testset, BATCH_SIZE, num_workers=CPUs)
+
+        loss_fn = nn.CrossEntropyLoss()
+        model = SignDigitsCNN()
+
+        NUM_EPOCHS = args.epochs
+        LR = args.lr
+
+        model, _ = util.training(model, train_dl, NUM_EPOCHS, loss_fn, LR)
+
+        torch.save(model.state_dict(), args.out_model)
+
+        acc = util.eval_cnn_classifier(model, test_dl, "cpu")
+        print("Accuracy of the network on the test images: {:.03f}%".format(acc))
+
+    elif subparser == "eval":
+
+        model.load_state_dict(torch.load(args.in_model))
+        acc = util.eval_cnn_classifier(model, test_dl, "cpu")
+        print("Accuracy of the network on the test images: {:.03f}%".format(acc))
 
 
 if __name__ == "__main__":
-    datapath = os.path.abspath("data")
-    dataset = CustomImageDataset(datapath)
-    data_len = len(dataset)
-    test_size = int(0.05 * data_len)
-    train_size = data_len - test_size
-
-    trainset, testset = random_split(dataset, [train_size, test_size])
-
-    BATCH_SIZE = 16
-    CPUs = os.cpu_count()
-    train_dl = DataLoader(trainset, BATCH_SIZE, True, num_workers=CPUs)
-    test_dl = DataLoader(testset, BATCH_SIZE, num_workers=CPUs)
-
-    loss_fn = nn.CrossEntropyLoss()
-    model = SignDigitsCNN()
-
-    NUM_EPOCHS = 10
-    LR = 2e-4
-
-    # model, loss_total = util.training(model, train_dl, NUM_EPOCHS, loss_fn, LR)
-
-    # torch.save(model.state_dict(), "model_cnn_classif.pt")
-
-    # acc = util.eval_cnn_classifier(model, test_dl, "cpu")
-    # print("Accuracy of the network on the test images: {:.03f}%".format(acc))
-
-    model.load_state_dict(torch.load("model_cnn_classif.pt"))
-    acc = util.eval_cnn_classifier(model, test_dl, "cpu")
-    print("Accuracy of the network on the test images: {:.03f}%".format(acc))
+    main()
---
+++ util/argparser.py
@@ -0,0 +1,78 @@
+import argparse
+import sys
+
+
+def get_cli_args():
+    args = sys.argv[1:]
+    argument_parser = argparse.ArgumentParser(
+        prog="sign digits detection",
+        description="Argument parser of the Sign Digits CNN detection",
+        allow_abbrev=True,
+    )
+    argument_parser.version = "0.1"
+
+    subparsers = argument_parser.add_subparsers()
+
+    train_subparser = subparsers.add_parser("train", help="Subparser for CNN training.")
+
+    train_subparser.add_argument(
+        "-i",
+        "--in_path",
+        action="store",
+        type=str,
+        help="Provide an input data path",
+        required=True,
+    )
+    train_subparser.add_argument(
+        "-o",
+        "--out_model",
+        action="store",
+        type=str,
+        help="Provide an output model path",
+        required=True,
+    )
+    train_subparser.add_argument(
+        "-s",
+        "--train_size",
+        action="store",
+        type=float,
+        help="Provide train data split as float",
+        required=True,
+    )
+    train_subparser.add_argument(
+        "-b",
+        "--batch_size",
+        action="store",
+        type=int,
+        help="Provide the size of mini-batch",
+        required=True,
+    )
+    train_subparser.add_argument(
+        "-x",
+        "--epochs",
+        action="store",
+        type=int,
+        help="Provide the amount of epochs to train",
+        required=True,
+    )
+    train_subparser.add_argument(
+        "-l",
+        "--lr",
+        action="store",
+        type=float,
+        help="Provide learning rate for classification model",
+        required=True,
+    )
+
+    eval_subparser = subparsers.add_parser("eval", help="Subparser for CNN evaluation.")
+
+    eval_subparser.add_argument(
+        "-m",
+        "--in_model",
+        action="store",
+        type=str,
+        help="Provide an input model path to load",
+        required=True,
+    )
+
+    return argument_parser.parse_args(args), args[0]
